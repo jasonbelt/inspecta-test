70693e61308db5d838d03eee8bcd6b87dd109227
Jason Belt <belt@ksu.edu>
2025-01-09 12:44:21 -0600

    fix script

--- .github/workflows/hamr/hamr-ci.cmd
+++ .github/workflows/hamr/hamr-ci.cmd
@@ -32,7 +32,7 @@ def findCIs(p: Os.Path): Unit = {
   if (!excludes.contains(p)) {
     if(p.isFile && p.name == "ci.cmd") {
       val r = proc"$sireum slang run $p".console.echo.run()
-      result = result + r.exitCodep
+      result = result + r.exitCode
     } else if(p.isDir) {
       p.list.foreach((m: Os.Path) => findCIs(m))
     }
